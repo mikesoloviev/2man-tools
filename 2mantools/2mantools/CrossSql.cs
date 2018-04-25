3f2707e1053f08f751c5559ccbd0b4363f730002
mikesoloviev <38215251+mikesoloviev@users.noreply.github.com>
2018-04-25 11:22:03 -0400

    v 04-25

--- 2mantools/2mantools/CrossSql.cs
+++ 2mantools/2mantools/CrossSql.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Text;
 using System.IO;
 
-// NOTE: In progress, most of the code -- just stubs.
+// NOTE: Implementation not completed.
 
 namespace X2MANTools {
 
     public class CrossSql {
+
+        Dictionary<string, string> mysqlTable = new Dictionary<string, string> {
+            { "IDENTITY", "AUTO_INCREMENT" }
+        };
+
+        Dictionary<string, string> sqliteTable = new Dictionary<string, string> {
+            { "IDENTITY", "AUTOINCREMENT" },
+            { "USE", "-- USE" }
+        };
+
+        Dictionary<string, string> oracleTable = new Dictionary<string, string> {
+            { "IDENTITY", "GENERATED ALWAYS AS IDENTITY" },
+            { "DATABASE", "SCHEMA" },
+            { "USE", "SET SCHEMA" }
+        };
 
         public void Transform(string sourcePath, string targetPath, string targetType) {
             var words = ParseToWords(File.ReadAllLines(sourcePath));
@@ -29,16 +44,45 @@ namespace X2MANTools {
 
         string[] ToMysql(string[] inWords) {
             var outWords = new List<string>();
+            foreach (var word in inWords) {
+                if (word.StartsWith("\""))
+                    outWords.Add(word.Replace('"', '`'));
+                else if (StartsWithLetter(word) && mysqlTable.ContainsKey(word))
+                    outWords.Add(mysqlTable[word]);
+                else
+                    outWords.Add(word);
+            }
             return outWords.ToArray();
         }
 
         string[] ToSqlite(string[] inWords) {
             var outWords = new List<string>();
+            for (var i = 0; i < inWords.Length; i++) {
+                var word = inWords[i];
+                if (word == "CREATE") {
+                    if (i < inWords.Length - 1 && inWords[i + 1] == "DATABASE")
+                        outWords.Add("-- " + word);
+                    else
+                        outWords.Add(word);
+                }
+                else if (StartsWithLetter(word) && sqliteTable.ContainsKey(word)) {
+                    outWords.Add(sqliteTable[word]);
+                }
+                else {
+                    outWords.Add(word);
+                }
+            }
             return outWords.ToArray();
         }
 
         string[] ToOracle(string[] inWords) {
             var outWords = new List<string>();
+            foreach (var word in inWords) {
+                if (StartsWithLetter(word) && oracleTable.ContainsKey(word))
+                    outWords.Add(oracleTable[word]);
+                else
+                    outWords.Add(word);
+            }
             return outWords.ToArray();
         }
 
@@ -46,7 +90,7 @@ namespace X2MANTools {
             var words = new List<string>();
             foreach (var line in lines) {
                 foreach (var token in line.Replace("(", " ( ").Replace(")", " ) ").Replace(",", " , ").Replace(";", " ; ").Split(' ')) {
-                    if (token == "") { /* skip */ };
+                    if (token == "") { /* skip */ }
                     else if (token.StartsWith("\"")) { words.Add(token); }
                     else { words.Add(token.ToUpper()); }
                 }
@@ -56,9 +100,37 @@ namespace X2MANTools {
 
         string FormatToText(string[] words) {
             var text = new StringBuilder();
+            for (var i = 0; i < words.Length; i++) {
+                switch (words[i]) {
+                    case "(":
+                        if (i > 0 && (words[i - 1].EndsWith("\"") || words[i - 1].EndsWith("`"))) {
+                            text.Append(" ");
+                            text.AppendLine(words[i]);
+                            text.Append(" ");
+                        }
+                        else {
+                            text.Append(words[i]);
+                        }
+                        break;
+                    case ")":
+                        text.Append(words[i]);
+                        break;
+                    case ",":
+                    case ";":
+                        text.AppendLine(words[i]);
+                        break;
+                    default:
+                        if (i == 0 || words[i - 1] != "(") text.Append(" ");
+                        text.Append(words[i]);
+                        break;
+                }
 
-            
-            retrun text.ToString();
+            }
+            return text.ToString();
+        }
+
+        bool StartsWithLetter(string s) {
+            return char.IsLetter(s[0]);
         }
 
     }
